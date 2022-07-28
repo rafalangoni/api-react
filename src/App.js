481f72102cd8dfddbb515505d1bd784f06fbc3b1
Rafael Langoni <langoninaves@gmail.com>
2022-07-28 16:29:33 -0300

    chamada funcionando

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ChamadaAPI from './componentes';
 
 function App() {
+
   return (
     <div className="App">
       <h1>Olá, mundo</h1>
+      <ChamadaAPI />
     </div>
   );
 }
---
+++ src/componentes/index.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+
+
+
+const ChamadaAPI = () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch('https://api.adviceslip.com/advice');
+            const response = data.json();
+            return response;
+        }
+        const resultado = fetchData()
+        return (
+            console.log(resultado)
+        )
+    }
+    )
+
+}
+
+export default ChamadaAPI;
